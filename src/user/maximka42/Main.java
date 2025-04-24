81bd5833235e2ab7f7fae2062134e6b8f3d760f2
sanee4ka <sashatop4ik56565@gmail.com>
2025-04-25 00:03:31 +0300

    Bug fix

--- src/user/maximka42/Main.java
+++ src/user/maximka42/Main.java
@@ -5,34 +5,40 @@ import java.util.Scanner;
 public class Main {
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
+        // получаем первое число
         System.out.println("первое число");
         int num1 = scanner.nextInt();
 
+        // получаем второе число
         System.out.println("второе счисло");
         int num2 = scanner.nextInt();
 
+        // получаем действие
         System.out.println("действие");
+        scanner.nextLine(); // input fix bug
         String action = scanner.nextLine();
 
+        // получаем результат
+        int result = getResult(action, num1, num2);
+
+        // выводим результат
+        System.out.println("ответ: " + result);
+    }
+
+    private static int getResult(String action, int num1, int num2) {
         int result;
 
         switch (action) {
 
-            case "+" -> {
-                result = num1 + num2;
-            }
+            case "+" -> result = num1 + num2;
 
-            case "-" -> {
-                result = num1 - num2;
-            }
+            case "-" -> result = num1 - num2;
 
-            case "*" -> {
-                result = num1 * num2;
-            }
+            case "*" -> result = num1 * num2;
 
             case "/" -> {
                 if (num2 == 0)
@@ -41,11 +47,9 @@ public class Main {
                 result = num1 / num2;
             }
 
-            default -> {
-                throw new IllegalArgumentException("ты чот не так написал");
-            }
+            default -> throw new IllegalArgumentException("ты чот не так написал");
         }
 
-            System.out.println("ответ: " + result);
+        return result;
     }
 }
