56da9c1e0a898c43bb58c8227f986b6597c8ccd6
maximka42 <chizichiyichiqzishiziqzi@gmail.com>
2025-04-24 23:20:29 +0300

    переписал на калькулятор по приколу

--- src/com/itproger/Main.java
+++ src/com/itproger/Main.java
@@ -1,9 +1,69 @@
 package com.itproger;
 
+  import java.util.Scanner;
+
 public class Main {
+
+
     public static void main(String[] args){
 
-        System.out.println("Hello World!");
+       Scanner scanner = new Scanner(System.in);
+
+        System.out.println("первое число");
+       int num1 = scanner.nextInt();
+
+        System.out.println("второе счисло");
+       int num2 = scanner.nextInt();
+
+       int res;
+
+        System.out.println("действие");
+        Scanner actionScanner = new Scanner(System.in);
+        String action = actionScanner.nextLine();
+
+        switch (action) {
+
+            case"+":
+                res = num1 + num2;
+                System.out.println("ответ " + res);
+                break;
+
+            case"-":
+                res = num1 - num2;
+                System.out.println("ответ " + res);
+                break;
+
+            case"*":
+                res = num1 * num2;
+                System.out.println("ответ " + res);
+                break;
+
+            case"/":
+                if (num2 == 0) {
+                    System.out.println("на ноль делить нельзя");
+
+                }else {
+                  res = num1 / num2;
+                System.out.println("ответ " + res);
+                }
+                break;
+
+            default:
+                System.out.println("ты чот не так написал");
+
+
+
+
+        }
+
+
+
+
+
+
+
+
 
     }
+
 }
